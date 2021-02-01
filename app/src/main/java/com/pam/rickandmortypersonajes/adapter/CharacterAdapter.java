1da3b3d65d1907781f9d6ea1336a381ca6b36738
LINDVIOR18 <msarpe@pentalog.com>
2021-02-02 01:31:17 +0200

    Fast Fix

--- app/src/main/java/com/pam/rickandmortypersonajes/adapter/CharacterAdapter.java
+++ app/src/main/java/com/pam/rickandmortypersonajes/adapter/CharacterAdapter.java
@@ -14,6 +14,11 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.pam.rickandmortypersonajes.DetailsActivity;
 import com.pam.rickandmortypersonajes.R;
 import com.pam.rickandmortypersonajes.entity.ResultCharacter;
+import com.pam.rickandmortypersonajes.entity.ResultEpisode;
+import com.pam.rickandmortypersonajes.util.RetrofitGenerator;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -69,7 +74,7 @@ public class CharacterAdapter extends RecyclerView.Adapter<CharacterAdapter.Char
     }
 
     static class CharacterViewHolder extends RecyclerView.ViewHolder {
-        TextView textViewName, textViewLocation;
+        TextView textViewName, textViewLocation, textViewEpisode;
 
         public CharacterViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -80,10 +85,12 @@ public class CharacterAdapter extends RecyclerView.Adapter<CharacterAdapter.Char
             ImageView imageViewProfile = itemView.findViewById(R.id.imageProfileAdapter);
             textViewName = itemView.findViewById(R.id.name);
             textViewLocation = itemView.findViewById(R.id.location);
+            textViewEpisode = itemView.findViewById(R.id.aboutEpisode);
 
 
             textViewName.setText(result.name);
             textViewLocation.setText(result.location.name);
+            getEpisodeData(getFirstEpisode(result.episode));
 
             Glide.with(itemView)
                     .load(result.image)
@@ -91,5 +98,35 @@ public class CharacterAdapter extends RecyclerView.Adapter<CharacterAdapter.Char
                     .into(imageViewProfile);
 
         }
+
+        private String getFirstEpisode(List<String> episode) {
+            return episode.get(0);
+        }
+
+        private void getEpisodeData(String url) {
+            String[] divURL = url.split("/");
+            String charID = divURL[divURL.length - 1];
+
+            RetrofitGenerator.getApiService().getEpisodeDetails(Integer.parseInt(charID))
+                    .enqueue(new Callback<ResultEpisode>() {
+                        @Override
+                        public void onResponse(@NonNull Call<ResultEpisode> call, @NonNull Response<ResultEpisode> response) {
+                            if (response.isSuccessful()) {
+                                if (response.body() != null) {
+                                    updateFirstEpisodeElement(response.body().name);
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(@NonNull Call<ResultEpisode> call, @NonNull Throwable t) {
+                        }
+                    });
+        }
+
+        private void updateFirstEpisodeElement(String name) {
+            textViewEpisode.setText(name);
+        }
     }
 }
+
--- app/src/main/java/com/pam/rickandmortypersonajes/adapter/DetailsAdapter.java
+++ app/src/main/java/com/pam/rickandmortypersonajes/adapter/DetailsAdapter.java
@@ -13,6 +13,11 @@ import com.bumptech.glide.Glide;
 import com.pam.rickandmortypersonajes.DetailsActivity;
 import com.pam.rickandmortypersonajes.R;
 import com.pam.rickandmortypersonajes.entity.ResultCharacter;
+import com.pam.rickandmortypersonajes.entity.ResultEpisode;
+import com.pam.rickandmortypersonajes.util.RetrofitGenerator;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -61,7 +66,8 @@ public class DetailsAdapter extends RecyclerView.Adapter<DetailsAdapter.Characte
             context.startActivity(intent);
         });
     }
-//    public void clear() {
+
+    //    public void clear() {
 //        int size = characterResults.size();
 //        characterResults.clear();
 //        notifyItemRangeRemoved(0, size);
@@ -72,7 +78,7 @@ public class DetailsAdapter extends RecyclerView.Adapter<DetailsAdapter.Characte
     }
 
     static class CharacterViewHolder extends RecyclerView.ViewHolder {
-        TextView textViewName, textViewLocation;
+        TextView textViewName, textViewLocation, textViewEpisode;
 
         public CharacterViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -83,15 +89,45 @@ public class DetailsAdapter extends RecyclerView.Adapter<DetailsAdapter.Characte
             ImageView imageViewProfile = itemView.findViewById(R.id.imageProfileAdapter);
             textViewName = itemView.findViewById(R.id.name);
             textViewLocation = itemView.findViewById(R.id.location);
-
+            textViewEpisode = itemView.findViewById(R.id.aboutEpisode);
 
             textViewName.setText(result.name);
             textViewLocation.setText(result.location.name);
+            getEpisodeData(getFirstEpisode(result.episode));
 
             Glide.with(itemView)
                     .load(result.image)
                     .into(imageViewProfile);
 
+        }
+
+        private String getFirstEpisode(List<String> episode) {
+            return episode.get(0);
+        }
+
+        private void getEpisodeData(String url) {
+            String[] divURL = url.split("/");
+            String charID = divURL[divURL.length - 1];
+
+            RetrofitGenerator.getApiService().getEpisodeDetails(Integer.parseInt(charID))
+                    .enqueue(new Callback<ResultEpisode>() {
+                        @Override
+                        public void onResponse(@NonNull Call<ResultEpisode> call, @NonNull Response<ResultEpisode> response) {
+                            if (response.isSuccessful()) {
+                                if (response.body() != null) {
+                                    updateFirstEpisodeElement(response.body().name);
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(@NonNull Call<ResultEpisode> call, @NonNull Throwable t) {
+                        }
+                    });
+        }
+
+        private void updateFirstEpisodeElement(String name) {
+            textViewEpisode.setText(name);
         }
     }
 }
